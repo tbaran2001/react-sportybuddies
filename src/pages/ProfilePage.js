bfed6b150c1b0becd07158b95c6633f7c0a93ce2
tbaran2001 <barantomasz2001@gmail.com>
2024-08-15 19:40:00 +0200

    Add functionality to add and remove sports on ProfilePage

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -4,31 +4,76 @@ import { useUser } from "../contexts/UserProvider";
 import { getGravatarUrl } from "../utils/gravatar";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import { useEffect, useState } from "react";
+import { useApi } from "../contexts/ApiProvider";
 
 export default function ProfilePage() {
   const { user } = useUser();
   const gravatarUrl = user ? getGravatarUrl(user.email, 512) : undefined;
+  const [sports, setSports] = useState([]);
+  const api = useApi();
+  const [userSports, setUserSports] = useState(user.sports);
+
+  const addSport = async (sport) => {
+    const response = await api.post(`/usersports/${user.id}/${sport.id}`);
+    if (response.ok) {
+      setUserSports([...userSports, sport]);
+    }
+  };
+
+  const removeSport = async (sport) => {
+    const response = await api.delete(`/usersports/${user.id}/${sport.id}`);
+    if (response.ok) {
+      setUserSports(userSports.filter((userSport) => userSport.id !== sport.id));
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      const response = await api.get("/sports");
+      if (response.ok) {
+        setSports(response.body);
+      } else {
+        setSports(null);
+      }
+    })();
+  }, [api]);
 
   return (
     <Body sidebar>
       {user === undefined ? (
         <Spinner animation="border" />
       ) : (
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src={gravatarUrl} />
-          <Card.Body>
-            <Card.Title>{user.userName}</Card.Title>
-            <Card.Text>
-              {user.description}
-            </Card.Text>
-            <Button variant="primary">Send message</Button> 
-            <ul>
-                {user.sports.map((sport) => (
-                    <li key={sport.id}>{sport.name}</li>
+        <>
+          <Card style={{ width: "18rem" }}>
+            <Card.Img variant="top" src={gravatarUrl} />
+            <Card.Body>
+              <Card.Title>{user.userName}</Card.Title>
+              <Card.Text>{user.description}</Card.Text>
+              <Button variant="primary">Send message</Button>
+              <ul>
+                {userSports.map((sport) => (
+                  <li key={sport.id}>{sport.name}</li>
                 ))}
-            </ul>
-          </Card.Body>
-        </Card>
+              </ul>
+            </Card.Body>
+          </Card>
+          <ul>
+            {sports.map((sport) => {
+              const userHasSport = userSports.some((userSport) => userSport.id === sport.id);
+              return (
+                <li key={sport.id}>
+                  {sport.name}
+                  {userHasSport ? (
+                    <button onClick={() => removeSport(sport)}>Remove</button>
+                  ) : (
+                    <button onClick={() => addSport(sport)}>Add</button>
+                  )}
+                </li>
+              );
+            })}
+          </ul>
+        </>
       )}
     </Body>
   );
