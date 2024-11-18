fd73c5834cee514d282f64c7c90a55f0ded09c4b
tbaran2001 <barantomasz2001@gmail.com>
2024-11-18 22:46:38 +0100

    Matching working

--- src/components/Pages/UserMatching/UserMatchingPage.js
+++ src/components/Pages/UserMatching/UserMatchingPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Stack, Box} from "@mui/material";
 import IconButton from "@mui/material/IconButton";
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
@@ -6,31 +6,52 @@ import ThumbDownOutlinedIcon from '@mui/icons-material/ThumbDownOutlined';
 import {styled} from "@mui/material/styles";
 import Body from "../../Body";
 import UserCard from "../../UserCard/UserCard";
+import {useApi} from "../../../contexts/ApiProvider";
+import {getRandomMatch, swipeMatch} from "../../../api/matching";
+import Spinner from "react-bootstrap/Spinner";
 
 const SwipeBox = styled(Box)(({theme}) => ({
-    flex:"1",
-    display:"flex",
-    justifyContent:"center",
-    alignItems:"center",
+    flex: "1",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
 }));
 
 export default function UserMatchingPage() {
+    const api = useApi();
+    const [match, setMatch] = useState();
+
+    useEffect(() => {
+        (async () => {
+            const match = await getRandomMatch(api);
+            setMatch(match);
+        })();
+    }, []);
+
+    const swipe = async (direction) => {
+        const newMatch = await swipeMatch(api, match.id, direction);
+        setMatch(newMatch);
+    };
 
     return (
         <Body sidebar>
-            <Stack flex={4} direction="row" spacing={2} justifyContent={"space-between"}>
-                <SwipeBox>
-                    <IconButton aria-label="Swipe Left" size="large">
-                        <ThumbDownOutlinedIcon fontSize="large"/>
-                    </IconButton>
-                </SwipeBox>
-                <UserCard/>
-                <SwipeBox>
-                    <IconButton aria-label="Swipe Right" size="large">
-                        <FavoriteBorderOutlinedIcon fontSize="large"/>
-                    </IconButton>
-                </SwipeBox>
-            </Stack>
+            {(match === undefined || match === null) ? (
+                <Spinner animation="border"/>
+            ) : (
+                <Stack flex={4} direction="row" spacing={2} justifyContent={"space-between"}>
+                    <SwipeBox>
+                        <IconButton onClick={() => swipe('left')} aria-label="Swipe Left" size="large">
+                            <ThumbDownOutlinedIcon fontSize="large"/>
+                        </IconButton>
+                    </SwipeBox>
+                    <UserCard user={match.matchedUser} userSports={match.matchedUser.sports}/>
+                    <SwipeBox>
+                        <IconButton onClick={() => swipe('right')} aria-label="Swipe Right" size="large">
+                            <FavoriteBorderOutlinedIcon fontSize="large"/>
+                        </IconButton>
+                    </SwipeBox>
+                </Stack>
+            )}
         </Body>
     );
 }
